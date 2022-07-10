ba809eca6b0d1595292b68765ed78b0065c08886
Joseph Barbone <JoeBarbone@aol.com>
2022-07-10 16:19:40 -0400

    update Get routes to include votes/update messy code

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,21 @@
+const sequelize = require('../../config/connection');
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
 
-// get all users
+
+
+
 router.get('/', (req, res) => {
     Post.findAll({
-      attributes: ['id', 'post_url', 'title', 'created_at'],
+      attributes: [
+        "id",
+        "post_url",
+        "title",
+        "created_at"]
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count'],
+        
       order: [["created_at", "DESC"]],
+      
       include: [
         {
           model: User,
@@ -20,12 +30,20 @@ router.get('/', (req, res) => {
       });
 });
 
+
+
 router.get('/:id', (req, res) => {
     Post.findOne({
       where: {
         id: req.params.id
       },
-      attributes: ['id', 'post_url', 'title', 'created_at'],
+      attributes: [
+        "id",
+        "post_url",
+        "title",
+        "created_at"]
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count'],
+
       include: [
         {
           model: User,
@@ -47,7 +65,8 @@ router.get('/:id', (req, res) => {
 });
 
 
-  router.post('/', (req, res) => {
+
+router.post('/', (req, res) => {
     // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
     Post.create({
       title: req.body.title,
@@ -60,6 +79,49 @@ router.get('/:id', (req, res) => {
         res.status(500).json(err);
       });
 });
+
+
+
+// PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+
+     // custom static method created in models/Post.js
+  Post.upvote(req.body, { Vote })
+  .then(updatedPostData => res.json(updatedPostData))
+  .catch(err => {
+    console.log(err);
+    res.status(400).json(err);
+  });
+    
+    // Vote.create({
+    //     user_id: req.body.user_id,
+    //     post_id: req.body.post_id
+    //   }).then(() => {
+    //     // then find the post we just voted on
+    //     return Post.findOne({
+    //       where: {
+    //         id: req.body.post_id
+    //       },
+    //       attributes: [
+    //         'id',
+    //         'post_url',
+    //         'title',
+    //         'created_at',
+    //         // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name `vote_count`
+    //         [
+    //           sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+    //           'vote_count'
+    //         ]
+    //       ]
+    //     })
+    //     .then(dbPostData => res.json(dbPostData))
+    //     .catch(err => {
+    //       console.log(err);
+    //       res.status(400).json(err);
+    //     });
+    // });
+});
+
 
 
 
@@ -89,6 +151,7 @@ router.put('/:id', (req, res) => {
 
 
 
+
 router.delete('/:id', (req, res) => {
     Post.destroy({
       where: {
@@ -107,6 +170,15 @@ router.delete('/:id', (req, res) => {
         res.status(500).json(err);
       });
 });
+
+
+
+
+
+
+
+
+
 
 
 module.exports = router;
